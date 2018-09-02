79f7c6fc22ba7a981a05d2363ab67e39149bc016
Yogi Butola <yogibutola@icloud.com>
2018-09-02 15:35:22 -0500

    Initial Commit

---
+++ src/main/java/com/butola/restcustomer/RestcustomerApplication.java
@@ -0,0 +1,12 @@
+package com.butola.restcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcustomerApplication.class, args);
+	}
+}
